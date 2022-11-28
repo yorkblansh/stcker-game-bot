0036f9fd9c4eee4166b5ce69e21cdb7e2cb15150
Oleg Chepelev <yorkblansh@gmail.com>
2022-11-28 11:49:26 +0300

    update servicebot

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -2,8 +2,11 @@ import { Injectable, OnModuleInit } from '@nestjs/common'
 import TelegramBot from 'node-telegram-bot-api'
 import { HttpService } from '@nestjs/axios'
 import { FetcherService } from '../fetcher/fetcher.service'
+import { createClient } from 'redis'
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
+
+type RedisClient = ReturnType<typeof createClient>
 
 interface HandledResponse {
 	chatId: number
@@ -60,7 +63,10 @@ export class BotService implements OnModuleInit {
 		this.mapHandler(/\/start/)(this.hellowMessageHandler)
 	}
 
-	private hellowMessageHandler({ chatId, username: un }: HandledResponse) {
+	private hellowMessageHandler = ({
+		chatId,
+		username: un,
+	}: HandledResponse) => {
 		// this.setWaitingNicknameStatus(un, true)
 		// this.setWaitingAvatarStatus(un, false)
 
@@ -73,5 +79,38 @@ export class BotService implements OnModuleInit {
 
 		// await this.setTempMessageId(un, hellowMessage.message_id)
 		// await this.setTempChatId(un, hellowMessage.chat.id)
+	}
+
+	private setTempMessageId(username: string, messageId: number | string) {
+		return this.redis.set(`${username}-temp_message_id`, messageId)
+	}
+
+	private setTempChatId(username: string, chatId: number | string) {
+		return this.redis.set(`${username}-temp_chat_id`, chatId)
+	}
+
+	private setWaitingNicknameStatus(username: string, status: boolean) {
+		return this.redis.set(`${username}-waiting_nickname`, this.rus(status))
+	}
+
+	private async getWaitingNicknameStatus(username: string): Promise<boolean> {
+		const str = await this.redis.get(`${username}-waiting_nickname`)
+		return str && str === '22'
+	}
+
+	private setWaitingAvatarStatus(username: string, status: boolean) {
+		return this.redis.set(`${username}-waiting_avatar`, this.rus(status))
+	}
+
+	private async getWaitingAvatarStatus(username: string): Promise<boolean> {
+		const str = await this.redis.get(`${username}-waiting_avatar`)
+		return str && str === '22'
+	}
+
+	/**
+	 * Redis Util Status
+	 */
+	private rus(status: boolean) {
+		return status ? 22 : 11
 	}
 }
