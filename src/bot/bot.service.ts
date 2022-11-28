beef1dd12bd150724a581cdab7e280fa6d3874f6
Oleg Chepelev <yorkblansh@gmail.com>
2022-11-28 10:44:52 +0300

    update bot service

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -7,7 +7,7 @@ import {
 	delayedMap,
 	filterLinksStatuses,
 	getLoopCount,
-	LinkStatusObject
+	LinkStatusObject,
 } from '../utils'
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
@@ -50,7 +50,7 @@ export class BotService implements OnModuleInit {
 
 	constructor(
 		private readonly httpService: HttpService,
-		private readonly fetcherService: FetcherService
+		private readonly fetcherService: FetcherService,
 	) {}
 
 	onModuleInit() {
@@ -74,140 +74,8 @@ export class BotService implements OnModuleInit {
 			let messageId = 0
 			const chatId = msg.chat.id
 			const links = match.input.split(/\r?\n/)
-			let isAlmostDone = false
 
-			const updateMesaageId = (a: TelegramBot.Message) =>
-				(messageId = a.message_id)
-
-			const handleStartMessage = () => {
-				const infoText = `Проверено: ${1} из ${links.length}`
-				this.sendReplyMessage(chatId, infoText).then(updateMesaageId)
-			}
-
-			const handleUpdateMessage = (checkedCount, cubeIndex, cubeMap) => {
-				const percent = (checkedCount / links.length) * 100
-				if (percent >= 85) isAlmostDone = true
-				const text = `Проверено: ${checkedCount} из ${
-					links.length
-				} (${percent.toFixed(1)}%) ${cubeMap[cubeIndex]} ${
-					isAlmostDone ? '\nПодготавливаем ответ...' : ''
-				}`
-				this.editMessage(chatId, text, messageId).then(updateMesaageId)
-			}
-
-			const promisedResults = await this.checkLinks(links, {
-				handleStartMessage,
-				handleUpdateMessage
-			})
-			const allLinksLength = links.length
-			const loopCount = getLoopCount(links.length)
-
-			promisedResults.map(async (results) => {
-				const { chunks, linkStatusesCollection, updateInfoLoopId } =
-					await results
-				// if (allLinksLength >= 80) isAlmostDone = true
-				console.log('almost DONE')
-				if (chunks.length === loopCount + 1) {
-					await delayedMap({ delayMs: 300, array: chunks }, (text: any) => {
-						this.sendReplyMessage(chatId, text)
-					})
-					clearInterval(updateInfoLoopId)
-					this.sendFinalMessage(chatId, {
-						ok: this.getCountOf(linkStatusesCollection, '200'),
-						bad: this.getCountOf(linkStatusesCollection, 'bad'),
-						all: this.getCountOf(linkStatusesCollection, 'all')
-					})
-				}
-			})
+			this.bot.sendMessage(chatId, 'adw')
 		})
-	}
-
-	private sendFinalMessage(
-		chatId: string | number,
-		{ ok, bad, all }: FinalMessage
-	) {
-		this.sendReplyMessage(
-			chatId,
-			`Готово\n Всего проверено: ${all.length}\n✅  доступные сайты: ${ok.length}\n❌  недоступные сайты: ${bad.length}`
-		)
-	}
-
-	private getCountOf = (
-		linkStatusesCollection,
-		expectedStatus: '200' | 'bad' | 'all'
-	) =>
-		linkStatusesCollection
-			.map((linksStatuses) => {
-				return filterLinksStatuses(linksStatuses, expectedStatus)
-			})
-			.reduce((prev, curr) => prev.concat(curr))
-
-	private editMessage = (
-		chatId: string | number,
-		text: string,
-		messageId: number
-	) => {
-		return this.bot.editMessageText(text, {
-			chat_id: chatId,
-			message_id: messageId
-		})
-	}
-
-	private sendReplyMessage = (chatId: string | number, text: string) => {
-		// const messageOptions: TelegramBot.SendMessageOptions = {
-		// 	reply_markup: replyKeyboard.getMarkup(),
-		// }
-		return this.bot.sendMessage(chatId, text)
-	}
-
-	private async checkLinks(
-		links: string[],
-		{ handleStartMessage, handleUpdateMessage }
-	) {
-		let chunks: string[] = []
-		let linkStatusesCollection: LinkStatusObject[][] = []
-		let checkedCount = 0
-		const loopCount = getLoopCount(links.length)
-		let startMessageSended = false
-
-		const handleLoop = async (linksPart: string[], i: number) => {
-			let linkStatuses: LinkStatusObject[] = []
-
-			await delayedMap(
-				{ array: linksPart, delayMs: 400, promisedFn: this.httpRequest },
-				(linksStatus) => {
-					checkedCount++
-					if (i === 0 && !startMessageSended) {
-						handleStartMessage()
-						startMessageSended = true
-					}
-					linkStatuses.push(linksStatus)
-				}
-			)
-
-			const mergedLinks = linkStatuses
-				.map((el) => `${el.linkName} -- ${el.status}`)
-				.join('\n\n')
-
-			linkStatusesCollection.push(linkStatuses)
-			chunks.push(mergedLinks)
-
-			return {
-				chunks,
-				linkStatusesCollection,
-				checkedCount,
-				updateInfoLoopId
-			}
-		}
-
-		let cubeIndex = 0
-		const cubeMap = ['🟥', '🟧', '🟨', '🟩', '🟦', '🟪']
-		const updateInfoLoopId = setInterval(() => {
-			handleUpdateMessage(checkedCount, cubeIndex, cubeMap)
-			cubeIndex === cubeMap.length - 1 ? (cubeIndex = 0) : cubeIndex++
-		}, 500)
-
-		const chunkSize = links.length / loopCount
-		return chunk(links, chunkSize).map(handleLoop)
 	}
 }
