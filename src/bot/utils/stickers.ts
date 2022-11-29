00926ea6a58b99c66135a2f69d3c9d0f95e31e8a
Oleg Chepelev <yorkblansh@gmail.com>
2022-11-29 11:40:30 +0300

    update stickers.ts

--- src/bot/utils/stickers.ts
+++ src/bot/utils/stickers.ts
@@ -1,4 +1,6 @@
 export const sticker = {
 	bunny_hellow:
 		'CAACAgIAAxkBAAEabWBjhL3pqEkWF-buuFtk68OShtO9wQACRQQAAsxUSQm4i2G1q8TXeisE',
+	nice_bunny:
+		'CAACAgIAAxkBAAEacCNjhR2EtbIY3x0obbM4sN4Cje-wmgACRgQAAsxUSQkJow-DWqQ5tSsE',
 }
