df2dd150a699dc573c11de07a8f14fb357e4078d
Oleg Chepelev <yorkblansh@gmail.com>
2022-12-26 16:09:35 +0300

    add stickers

--- bot/workspaces/tgbot/src/bot/utils/stickers.ts
+++ bot/workspaces/tgbot/src/bot/utils/stickers.ts
@@ -1,3 +1,10 @@
+const pets = {
+	pug_default:
+		'CAACAgIAAxkBAAEbHbdjnw18R_YdKP-lF6tazTx2NQZbYQACHgADnP4yMNprdIJofH5sLAQ',
+	spider:
+		'CAACAgIAAxkBAAEbHbljnw3DZw5sHm19ovvDav-e1vwL0AACEAADfoTDCMFOAiohk74TLAQ',
+}
+
 export const sticker = {
 	bunny_hellow:
 		'CAACAgIAAxkBAAEabWBjhL3pqEkWF-buuFtk68OShtO9wQACRQQAAsxUSQm4i2G1q8TXeisE',
@@ -11,4 +18,5 @@ export const sticker = {
 		'CAACAgIAAxkBAAEakSFjieMtJiGPSd7Dn8LmPvj3B5ABGQACRAQAAsxUSQm_fwr0h1sdvisE',
 	postoyalets:
 		'CAACAgIAAxkBAAEapZ5jjX69oZwh-PfhV1yFqyS1wePpfQACLg0AAk77QEk1lx4P0Q3fOysE',
+	pets,
 }
