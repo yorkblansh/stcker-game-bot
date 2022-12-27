59fe22a4e7381097474067c9447a691053908826
Oleg Chepelev <yorkblansh@gmail.com>
2022-12-27 09:47:07 +0300

    add shared content

---
+++ bot/workspaces/tgbot/src/shared/SocketIOEvents.ts
@@ -0,0 +1,8 @@
+export const SocketIOEvents = {
+	_user_update: (assembledEvent: string) => `${assembledEvent}_user_update`,
+}
+
+// = (assembledEvent: string) => ({
+// 	_user_update: `${assembledEvent}_user_update`,
+
+// })
