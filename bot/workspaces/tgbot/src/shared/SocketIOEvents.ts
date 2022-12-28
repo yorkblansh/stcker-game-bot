1676e01c76889098e9ab683be48844cc1065c505
Oleg Chepelev <yorkblansh@gmail.com>
2022-12-28 15:48:45 +0300

    update socket io Events

--- bot/workspaces/tgbot/src/shared/SocketIOEvents.ts
+++ bot/workspaces/tgbot/src/shared/SocketIOEvents.ts
@@ -1,4 +1,6 @@
 export const SOCKET_IO_EVENTS = (sharedEvent: string) => ({
 	_user_update: `${sharedEvent}_user_update`,
 	_ready2fight: `${sharedEvent}_ready2fight`,
+	_ready: `${sharedEvent}_ready`,
+	_damager_username: `${sharedEvent}_damage`,
 })
