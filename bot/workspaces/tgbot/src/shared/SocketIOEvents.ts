6462b13def129800dcdc9702c226ac18efcbc0cd
Oleg Chepelev <yorkblansh@gmail.com>
2022-12-28 15:41:55 +0300

    rf

--- bot/workspaces/tgbot/src/shared/SocketIOEvents.ts
+++ bot/workspaces/tgbot/src/shared/SocketIOEvents.ts
@@ -1,4 +1,4 @@
-export const SOCKET_IO_EVENTS = (assembledEvent: string) => ({
-	_user_update: `${assembledEvent}_user_update`,
-	_ready2fight: `${assembledEvent}_ready2fight`,
+export const SOCKET_IO_EVENTS = (sharedEvent: string) => ({
+	_user_update: `${sharedEvent}_user_update`,
+	_ready2fight: `${sharedEvent}_ready2fight`,
 })
