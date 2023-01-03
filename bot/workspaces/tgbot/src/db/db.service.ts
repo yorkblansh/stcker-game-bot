337f426a513c636589dcdf5092b176f8866d4958
Oleg Chepelev <yorkblansh@gmail.com>
2023-01-03 19:43:53 +0300

    refactor

--- bot/workspaces/tgbot/src/db/db.service.ts
+++ bot/workspaces/tgbot/src/db/db.service.ts
@@ -1,6 +1,11 @@
 import { Either, right, left } from '@sweet-monads/either'
 import { RedisClient } from '../bot/bot.service'
 
+enum MethodsFactoryLiteralReturnType {
+	'RETURN_STRING',
+	'RETURN_MONAD',
+}
+
 type GetOrSet = 'get' | 'set'
 
 enum Postfix {
@@ -16,9 +21,7 @@ enum Postfix {
 	'-assembled-event' = '-assembled-event',
 }
 
-const monadPredicat = (str: string) => str && str === '22'
-
-const rus = (
+const dataMapToDb = (
 	value:
 		| boolean
 		| string
@@ -38,27 +41,30 @@ export class DbService {
 		private readonly username: string,
 	) {}
 
-	private dbMethodsFactory = <
-		WIN extends 'getMonad' | 'getString',
-		MonadOrString = WIN extends 'getMonad'
+	private monadPredicat = (str: string) => str && str === '22'
+
+	private methodsFactory = <
+		LiteralReturnType extends keyof typeof MethodsFactoryLiteralReturnType,
+		MonadOrString = LiteralReturnType extends MethodsFactoryLiteralReturnType.RETURN_MONAD
 			? Either<boolean, boolean>
 			: string,
 	>(
 		postfix: keyof typeof Postfix,
-		whatIsNeed: WIN,
-		monadPredicatCB?: (arg: any) => any,
+		literalReturnType: LiteralReturnType,
 	) => {
+		const monadPredicat = this.monadPredicat
 		const redis = this.redis
 		const redisArg = this.username + postfix
+
 		function fn(
-			queryType: 'set',
+			getOrSet: 'set',
 			value:
 				| boolean
 				| string
 				| number
 				| (number | string | NodeJS.Timer)[],
 		): Promise<string>
-		function fn(queryType: 'get'): Promise<MonadOrString>
+		function fn(getOrSet: 'get'): Promise<MonadOrString>
 		function fn<
 			GoS extends GetOrSet,
 			ReturnType = GoS extends 'get'
@@ -67,16 +73,17 @@ export class DbService {
 		>(getOrSet: GetOrSet, value?: string): ReturnType {
 			return {
 				get: async () => {
-					const str = await redis.get(redisArg)
+					const redisResult = await redis.get(redisArg)
 					return (
-						whatIsNeed === 'getMonad'
-							? monadPredicatCB(str)
+						literalReturnType === 'RETURN_MONAD'
+							? monadPredicat(redisResult)
 								? right(true)
 								: left(false)
-							: str
+							: redisResult
 					) as MonadOrString
 				},
-				set: async () => await redis.set(redisArg, rus(value)),
+				set: async () =>
+					await redis.set(redisArg, dataMapToDb(value)),
 			}[getOrSet]() as ReturnType
 		}
 
@@ -90,47 +97,54 @@ export class DbService {
 				status,
 			},
 		})
-		this.redis.set(`${username}-waiting_start_hello`, rus(status))
+		this.redis.set(
+			`${username}-waiting_start_hello`,
+			dataMapToDb(status),
+		)
 	}
 
-	tempChatId = this.dbMethodsFactory('-temp_chat_id', 'getString')
-	nickname = this.dbMethodsFactory('-nickname', 'getString')
-	startHelloStatus = this.dbMethodsFactory(
+	tempChatId = this.methodsFactory('-temp_chat_id', 'RETURN_STRING')
+
+	nickname = this.methodsFactory('-nickname', 'RETURN_STRING')
+
+	startHelloStatus = this.methodsFactory(
 		'-waiting_start_hello',
-		'getMonad',
-		monadPredicat,
+		'RETURN_MONAD',
 	)
-	nicknameStatusRepeated = this.dbMethodsFactory(
+
+	nicknameStatusRepeated = this.methodsFactory(
 		'-waiting_nickname_repeated',
-		'getMonad',
-		monadPredicat,
+		'RETURN_MONAD',
 	)
-	nicknameStatus = this.dbMethodsFactory(
+
+	nicknameStatus = this.methodsFactory(
 		'-waiting_nickname',
-		'getMonad',
-		monadPredicat,
+		'RETURN_MONAD',
 	)
-	avatarStatus = this.dbMethodsFactory(
+
+	avatarStatus = this.methodsFactory(
 		'-waiting_avatar',
-		'getMonad',
-		monadPredicat,
+		'RETURN_MONAD',
 	)
-	tempMessageIdList = this.dbMethodsFactory(
+
+	tempMessageIdList = this.methodsFactory(
 		'-temp-message-id-list',
-		'getString',
+		'RETURN_STRING',
 	)
-	tempIntervalTimerList = this.dbMethodsFactory(
+
+	tempIntervalTimerList = this.methodsFactory(
 		'-interval-timer-list',
-		'getString',
+		'RETURN_STRING',
 	)
-	assembledEvent = this.dbMethodsFactory(
+
+	assembledEvent = this.methodsFactory(
 		'-assembled-event',
-		'getString',
+		'RETURN_STRING',
 	)
-	villageHintStatus = this.dbMethodsFactory(
+
+	villageHintStatus = this.methodsFactory(
 		'-village-hint-method-status',
-		'getMonad',
-		monadPredicat,
+		'RETURN_MONAD',
 	)
 
 	FLUSH_ALL = () => this.redis.flushAll()
